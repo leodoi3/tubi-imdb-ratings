dd3a1373f2b5f50ab3cc99ede10aef4d4a5296ed
Matt <matteochirco@gmail.com>
2022-09-05 09:28:06 -0700

    wait for pagelod and scroll properly

--- userscript.js
+++ userscript.js
@@ -7,39 +7,81 @@
 // @match        https://tubitv.com/movies/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @grant        none
+// @require      file:///C:\Users\matte\Repos\tubi-imdb-ratings\userscript.js
 // ==/UserScript==
 
-window.addEventListener('load', function () {
-  //var url = window.location.href;
-  //if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
-  setTimeout(addRecommendedButtons, 3000);
+// window.addEventListener('load', function () {
+//   //var url = window.location.href;
+//   //if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
+//   setTimeout(addRecommendedButtons, 3000);
+// this.alert("Hello")
+// }, false);
 
-}, false);
 
-function addRecommendedButtons() {
-  const content = document.getElementsByClassName("Row Carousel__row")[0].childNodes;
-  const nextBtn = document.getElementsByClassName("Button Button--secondary Button--round Carousel__next Carousel__arrow-active")[0];
-  const prevBtn = document.getElementsByClassName("Button Button--secondary Button--round Carousel__prev Carousel__arrow-active")[0];
-  nextBtn.click();
-  setTimeout(nextBtn.click(), 500);
-  setTimeout(nextBtn.click(), 1000);
 
-  setTimeout(() => {
-    for (let item of content) {
-      try {
-        addButton(item);
-      }
-      catch (e) { console.log(e) }
-      ;
+waitForElm("div:nth-child(5) > div").then((elm) => {
+  setTimeout(addRecommendedButtons, 1000);
+});
+
+waitForElm("div:nth-child(5) > div").then((elm) => {
+  //setTimeout(addRecommendedButtons, 1000);
+});
+
+function waitForElm(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
     }
-  }, 5000)
 
-    setTimeout(prevBtn.click(), 10000)
-    setTimeout(prevBtn.click(), 12000)
-    setTimeout(prevBtn.click(), 15000)
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
 
-
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function addRecommendedButtons() {
+  const content = document.getElementsByClassName("Row Carousel__row")[0].childNodes;
+  const nextBtn = document.querySelector(".Button--round.Carousel__next.Carousel__arrow-active");
+
+
+  for (i = 0; i < 4; i++) {
+    if (nextBtn) {
+      nextBtn.click()
+      await sleep(500)
+    }
+
+  }
+
+  for (let item of content) {
+    try {
+      addButton(item);
+    }
+    catch (e) { console.log(e) }
+    ;
+  }
+
+  const prevBtn = document.querySelector(".Button--round.Carousel__prev.Carousel__arrow-active");;
+  for (i = 0; i < 4; i++) {
+    if (prevBtn) {
+      prevBtn.click()
+      await sleep(500)
+    }
+  }
+}
+
+
 
 function addButton(item) {
   var title = getTitle(item);
@@ -54,8 +96,7 @@ function addButton(item) {
       btn.setAttribute('title', data.plot)
 
     }
-    else
-    {
+    else {
       btn.onclick = () => { window.open("https://www.google.com/search?q=" + title + " " + year) }
       btn.innerHTML = "Google Search: " + title + " " + year;
 
@@ -83,20 +124,23 @@ async function getRating(title, year) {
 }
 
 function getTitle(params) {
-  return params.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
+ // params.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
+  return params.querySelector("h3 > a").innerText
+  
 
 }
 
 function getYear(params) {
   let year = params.childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
+  if (!year.match(/\(\d+\)/)) return ""
   year = year.replace("(", "")
   year = year.replace(")", "")
   return year;
 }
 
-var url = window.location.href;
-if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
-  addRecommendedButtons()
+// var url = window.location.href;
+// if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
+//   addRecommendedButtons()
 
 
 
