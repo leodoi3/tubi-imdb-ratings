25d78ea7881424bcf75b4a0cb7690c1661ded0b5
Matt <matteochirco@gmail.com>
2022-09-04 23:40:25 -0700

    Show ratings on recommendations

--- userscript.js
+++ userscript.js
@@ -1,22 +1,71 @@
-var content =
-  document.getElementsByClassName("Row Carousel__row")[0].childNodes;
+// ==UserScript==
+// @name         IMDB Ratings on Tubi
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://tubitv.com/movies/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
+// @grant        none
+// ==/UserScript==
 
-for (let item of content) {
-  addButton(item);
+window.addEventListener('load', function () {
+  //var url = window.location.href;
+  //if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
+  setTimeout(addRecommendedButtons, 3000);
+
+}, false);
+
+function addRecommendedButtons() {
+  const content = document.getElementsByClassName("Row Carousel__row")[0].childNodes;
+  const nextBtn = document.getElementsByClassName("Button Button--secondary Button--round Carousel__next Carousel__arrow-active")[0];
+  const prevBtn = document.getElementsByClassName("Button Button--secondary Button--round Carousel__prev Carousel__arrow-active")[0];
+  nextBtn.click();
+  setTimeout(nextBtn.click(), 500);
+  setTimeout(nextBtn.click(), 1000);
+
+  setTimeout(() => {
+    for (let item of content) {
+      try {
+        addButton(item);
+      }
+      catch (e) { console.log(e) }
+      ;
+    }
+  }, 5000)
+
+    setTimeout(prevBtn.click(), 10000)
+    setTimeout(prevBtn.click(), 12000)
+    setTimeout(prevBtn.click(), 15000)
+
+
 }
 
 function addButton(item) {
-var title = getTitle(item);
-var year = getYear(item);
+  var title = getTitle(item);
+  var year = getYear(item);
+  var btn = document.createElement("button");
 
-//console.log(title)
-   getRating(title, year).then((data) => {
-    if (data != undefined)
-    { var btn = document.createElement("button");
-     btn.innerHTML = data.rating + data.title;
-     item.appendChild(btn);}
+  //console.log(title)
+  getRating(title, year).then((data) => {
+    if (data.title != undefined) {
+      btn.innerHTML = data.rating + "/10" + "<br>" + data.votes + " votes";
+      btn.onclick = () => { window.open("https://www.imdb.com/title/" + data.id) }
+      btn.setAttribute('title', data.plot)
 
-   })
+    }
+    else
+    {
+      btn.onclick = () => { window.open("https://www.google.com/search?q=" + title + " " + year) }
+      btn.innerHTML = "Google Search: " + title + " " + year;
+
+    }
+    btn.style.backgroundColor = "yellow";
+    btn.style.color = "black"
+    item.appendChild(btn);
+
+  }
+  )
 
 }
 
@@ -24,32 +73,32 @@ async function getRating(title, year) {
   const response = await fetch("https://www.omdbapi.com/?apikey=57c52853" + "&t=" + title + "&y=" + year)
   const json = await response.json();
 
-  return {title: json.Title ,
+  return {
+    title: json.Title,
     rating: json.imdbRating,
-         id: json.imdbID};
+    id: json.imdbID,
+    votes: json.imdbVotes,
+    plot: json.Plot
+  };
 }
 
-function getTitle(params){
-return params.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
+function getTitle(params) {
+  return params.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
 
 }
 
-function getYear(params){
+function getYear(params) {
   let year = params.childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText
-  year = year.replace("(","")
-  year = year.replace(")","")
-return year;}
+  year = year.replace("(", "")
+  year = year.replace(")", "")
+  return year;
+}
 
-// fetch("https://www.omdbapi.com/?apikey=57c52853&t=the%20crumbs&y=2020")
-//   .then(response => {
-//     // indicates whether the response is successful (status code 200-299) or not
-//     if (!response.ok) {
-//       throw new Error(`Request failed with status ${reponse.status}`)
-//     }
-//     return response.json()
-//   })
-//   .then(data => {
-//     console.log(data.count)
-//     console.log(data.products)
-//   })
-//   .catch(error => console.log(error))
+var url = window.location.href;
+if (url.match("https:\/\/tubitv.com\/movies\/\d+") || url.match("127.0.0.1"))
+  addRecommendedButtons()
+
+
+
+
+
